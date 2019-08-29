9c876499af29f80bd2d02fefbd8b1575586ef414
monica835 <monica.diglasa@student.passerellesnuemriques.org>
2019-08-29 20:30:24 +0800

    ongoing project

---
+++ login.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    function showhide(show,hide,hide1){
+        $('input').val(null)
+        $(show).show();
+        $(hide).hide();
+        $(hide1).hide1();
+    }
+    
+})
